92576030a6ff96fc43606e5001ce5ae132fd3e15
Frank Strieter <fs@co-it.eu>
2022-03-10 16:13:09 +0100

    chore: adjust to my needs

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -5,4 +5,5 @@ if [[ $OSTYPE == darwin* ]]; then
   verbose Setting up $fg[red]docker$reset_color aliases
 
   alias moby='docker run --rm -it --privileged --pid=host walkerlee/nsenter -t 1 -m -u -i -n sh'
+  alias drmia='docker rmi -f $(docker images -aq)'
 fi
