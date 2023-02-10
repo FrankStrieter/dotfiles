14631f2a9333172130e7f18277d04ea190d2803a
Frank Strieter <fs@co-it.eu>
2023-02-10 09:11:18 +0100

    chore: replace nonorc zpulg with zinit

--- wsl/wsl.zsh
+++ wsl/wsl.zsh
@@ -1,5 +1,4 @@
 
-set -x
 if grep --quiet microsoft /proc/version 2>/dev/null; then
 		 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
      export NVM_DIR=~/.nvm
@@ -9,4 +8,3 @@ if grep --quiet microsoft /proc/version 2>/dev/null; then
     export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
     sudo /etc/init.d/dbus start &> /dev/null
 fi
-set +x
